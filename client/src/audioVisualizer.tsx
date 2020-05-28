edd94ea8d7a713273c9ebecb31b9bcf32c528e81
Leo Gharib <leonargharib@gmail.com>
2020-05-28 02:50:48 -0700

    copying tracks to tracklist working

--- client/src/audioVisualizer.tsx
+++ client/src/audioVisualizer.tsx
@@ -3,11 +3,15 @@ import { UserFiles, ItemTypes, DragItem, DragItemTrack } from "./types";
 import { WaveformItem } from "./waveformItem";
 import { useDrop, XYCoord, DropTargetMonitor, useDrag } from "react-dnd";
 import update from "immutability-helper";
+import WaveformData from "waveform-data";
+import { v4 as uuidv4 } from "uuid";
 
 interface IAudioVisualizerProps {
   userFiles: UserFiles;
   style?: React.CSSProperties;
 }
+
+// todo create context with user files
 
 export const AudioVisualizer = (props: IAudioVisualizerProps) => {
   const userFilesArr = Object.values(props.userFiles);
@@ -87,6 +91,7 @@ export const AudioVisualizer = (props: IAudioVisualizerProps) => {
     accept: ItemTypes.BOX,
     drop(item: DragItem, monitor) {
       const delta = monitor.getDifferenceFromInitialOffset() as XYCoord;
+      if (!delta) return;
       const left = Math.round(item.left + delta.x);
       const top = Math.round(item.top + delta.y);
       moveBox(item.id, left, top);
@@ -126,28 +131,92 @@ export const AudioVisualizer = (props: IAudioVisualizerProps) => {
         );
       })}
       <div style={{ flex: 1 }} />
-      <AudioTrackList />
+      <AudioTrackList userFiles={props.userFiles} />
     </div>
   );
 };
 
 interface ITrack {
   id: string;
+  waveformData: WaveformData;
+}
+interface IAudioTrackListProps {
+  userFiles: UserFiles;
 }
 
-const AudioTrackList = () => {
-  //   const [tracks, setTracks] = useState<ITrack[]>([]);
-  const [tracks, setTracks] = useState<ITrack[]>([
-    {
-      id: "asdf",
-    },
-    {
-      id: "asdfsss",
-    },
-    {
-      id: "qqqq",
-    },
-  ]);
+const AudioTrackList = (props: IAudioTrackListProps) => {
+  const [tracks, setTracks] = useState<ITrack[]>([]);
+
+  const [boxes, setBoxes] = useState<{
+    [key: string]: {
+      top: number;
+      left: number;
+    };
+  }>({});
+
+  const canvasRefs: React.Ref<HTMLCanvasElement>[] = Array.from({
+    length: tracks.length,
+  }).map((_) => {
+    return React.createRef();
+  });
+
+  useEffect(() => {
+    if (tracks.length) {
+      renderCanvas();
+    }
+  }, [tracks]);
+
+  const renderCanvas = useCallback(() => {
+    const newBoxes: {
+      [key: string]: {
+        top: number;
+        left: number;
+      };
+    } = {};
+    tracks.forEach((track, index) => {
+      const { waveformData, id } = track;
+      if (!boxes[id])
+        newBoxes[id] = {
+          top: 0,
+          left: 0,
+        };
+      const scaleY = (amplitude: number, height: number) => {
+        const range = 256;
+        const offset = 128;
+
+        return height - ((amplitude + offset) * height) / range;
+      };
+
+      //@ts-ignore
+      const canvas = canvasRefs[index].current;
+      if (!canvas) return;
+
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+      ctx.beginPath();
+
+      const channel = waveformData.channel(0);
+
+      // Loop forwards, drawing the upper half of the waveform
+      for (let x = 0; x < waveformData.length; x++) {
+        const val = channel.max_sample(x);
+
+        ctx.lineTo(x + 0.5, scaleY(val, canvas.height) + 0.5);
+      }
+
+      // Loop backwards, drawing the lower half of the waveform
+      for (let x = waveformData.length - 1; x >= 0; x--) {
+        const val = channel.min_sample(x);
+
+        ctx.lineTo(x + 0.5, scaleY(val, canvas.height) + 0.5);
+      }
+
+      ctx.closePath();
+      ctx.stroke();
+      ctx.fill();
+    });
+    setBoxes(update(boxes, { $merge: newBoxes }));
+  }, [tracks]);
 
   const moveTrack = useCallback(
     (dragIndex: number, hoverIndex: number) => {
@@ -164,25 +233,60 @@ const AudioTrackList = () => {
     [tracks]
   );
 
+  const onDrop = (item: DragItem, monitor: DropTargetMonitor) => {
+    const newTrack = props.userFiles[item.id];
+    setTracks(tracks.concat({ ...newTrack, id: uuidv4() }));
+  };
+
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: ItemTypes.BOX,
+    drop(item, monitor) {
+      //@ts-ignore
+      onDrop(item, monitor);
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  const isActive = canDrop && isOver;
+
   return (
-    <div
-      style={{
-        width: "100%",
-        height: 170,
-        border: "2px dotted black",
-        display: "flex",
-        flexDirection: "row",
-      }}
-    >
-      {tracks.map((track, i) => (
-        <AudioTrack
-          id={track.id}
-          key={track.id}
-          index={i}
-          moveTrack={moveTrack}
-        />
-      ))}
-    </div>
+    <>
+      {isActive && (
+        <div
+          style={{
+            fontSize: "1em",
+            textAlign: "center",
+            color: "blue",
+          }}
+        >
+          copy track
+        </div>
+      )}
+      <div
+        style={{
+          width: "100%",
+          height: 150,
+          border: "2px dotted black",
+          display: "flex",
+          flexDirection: "row",
+          backgroundColor: isActive ? "lightblue" : "inherit",
+        }}
+        ref={drop}
+      >
+        {tracks.map((track, i) => (
+          <AudioTrack
+            id={track.id}
+            key={track.id}
+            index={i}
+            moveTrack={moveTrack}
+            ref={canvasRefs[i]}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
@@ -192,82 +296,86 @@ interface IAudioTrackProps {
   moveTrack: (dragIndex: number, hoverIndex: number) => void;
 }
 
-const AudioTrack = (props: IAudioTrackProps) => {
-  const { index, id, moveTrack } = props;
+const AudioTrack = React.forwardRef(
+  (props: IAudioTrackProps, canvasRef: React.Ref<HTMLCanvasElement>) => {
+    const { index, id, moveTrack } = props;
 
-  const ref = useRef<HTMLDivElement>(null);
-  const [, drop] = useDrop({
-    accept: ItemTypes.TRACK,
-    hover(item: DragItemTrack, monitor: DropTargetMonitor) {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
+    const ref = useRef<HTMLDivElement>(null);
+    const [, drop] = useDrop({
+      accept: ItemTypes.TRACK,
+      hover(item: DragItemTrack, monitor: DropTargetMonitor) {
+        if (!ref.current) {
+          return;
+        }
+        const dragIndex = item.index;
+        const hoverIndex = index;
 
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        return;
-      }
+        // Don't replace items with themselves
+        if (dragIndex === hoverIndex) {
+          return;
+        }
 
-      // Determine rectangle on screen
-      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+        // Determine rectangle on screen
+        const hoverBoundingRect = ref.current?.getBoundingClientRect();
 
-      // Get horizontal middle
-      const hoverMiddleX =
-        (hoverBoundingRect.left - hoverBoundingRect.right) / 2;
+        // Get horizontal middle
+        const hoverMiddleX =
+          (hoverBoundingRect.left - hoverBoundingRect.right) / 2;
 
-      // Determine mouse position
-      const clientOffset = monitor.getClientOffset();
+        // Determine mouse position
+        const clientOffset = monitor.getClientOffset();
 
-      // Get pixels to the right
-      const hoverClientX =
-        (clientOffset as XYCoord).x - hoverBoundingRect.right;
+        // Get pixels to the right
+        const hoverClientX =
+          (clientOffset as XYCoord).x - hoverBoundingRect.right;
 
-      // Only perform the move when the mouse has crossed half of the items width
+        // Only perform the move when the mouse has crossed half of the items width
 
-      // Dragging left
-      if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
-        return;
-      }
+        // Dragging left
+        if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
+          return;
+        }
 
-      // Dragging right
-      if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
-        return;
-      }
+        // Dragging right
+        if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
+          return;
+        }
 
-      // Time to actually perform the action
-      moveTrack(dragIndex, hoverIndex);
+        // Time to actually perform the action
+        moveTrack(dragIndex, hoverIndex);
 
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
-      item.index = hoverIndex;
-    },
-  });
+        // Note: we're mutating the monitor item here!
+        // Generally it's better to avoid mutations,
+        // but it's good here for the sake of performance
+        // to avoid expensive index searches.
+        item.index = hoverIndex;
+      },
+    });
 
-  const [{ isDragging }, drag] = useDrag({
-    item: { type: ItemTypes.TRACK, id, index },
-    collect: (monitor: any) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
+    const [{ isDragging }, drag] = useDrag({
+      item: { type: ItemTypes.TRACK, id, index },
+      collect: (monitor: any) => ({
+        isDragging: monitor.isDragging(),
+      }),
+    });
 
-  const opacity = isDragging ? 0 : 1;
-  drag(drop(ref));
-  return (
-    <div
-      ref={ref}
-      style={{
-        opacity,
-        border: "1px solid green",
-        padding: 10,
-        marginRight: 5,
-      }}
-    >
-      {/* <div ref={ref} style={{ ...style, opacity }}> */}
-      {id}
-    </div>
-  );
-};
+    const opacity = isDragging ? 0 : 1;
+    drag(drop(ref));
+    return (
+      <div
+        ref={ref}
+        style={{
+          opacity,
+          //   margin: 5,
+        }}
+      >
+        <canvas
+          ref={canvasRef}
+          width={200}
+          height={150}
+          style={{ border: "1px solid black" }}
+        />
+      </div>
+    );
+  }
+);
