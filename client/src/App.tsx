dfaa92c7b818f41d27cf6ed9ed4a1914a6b25ae7
Leo Gharib <lgharib@zoiclabs.com>
2020-08-20 02:30:27 -0700

    connect socket.io to heroku frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,12 +11,24 @@ import { DndProvider } from "react-dnd-multi-backend";
 import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch";
 import { Homepage } from "./routes/Homepage";
 import { PlayerPage } from "./routes/PlayerPage";
-import React from "react";
+import React, { useEffect } from "react";
 import { YoutubePage } from "./routes/YoutubePage";
 import backgroundImage from "./assets/Polka-Dots.svg";
 import { withFirebaseContext } from "./contexts/firebaseContext";
 
+import io from "socket.io-client";
+
+const socket = io("ws://yeeplayer.herokuapp.com");
+
 function App() {
+  useEffect(() => {
+    socket.on("connect", function () {
+      console.log("connected client");
+    });
+    socket.on("event", function (data: any) {});
+    socket.on("disconnect", function () {});
+  }, []);
+
   return (
     <div
       className="App"
