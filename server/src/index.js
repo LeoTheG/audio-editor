1f788acec9f98ea5c44ed358c920041e8e6da543
Leo Gharib <leonargharib@gmail.com>
2020-08-24 20:05:20 -0700

    server room detects

--- server/src/index.js
+++ server/src/index.js
@@ -3,36 +3,62 @@ const io = require("socket.io")(server);
 const port = process.env.PORT || 8000;
 
 const clientPositions = {};
+// {[clientId: string]: string}
+const clientRooms = {};
 
 // todo: emit client disconnects and room changes, should hide for former roommates
 
 io.on("connection", (client) => {
-  console.log("connected with", client);
   client.on("connect room", (id) => {
+    console.log("connecting to room ", id);
+
+    client.join(id);
+    clientRooms[client.id] = id;
     // leave old rooms
     for (room in client.rooms) {
-      if (client.id !== room) client.leave(room);
+      if (client.id !== room && id !== room) {
+        client.to(room).emit("roommate disconnect", client.id);
+        client.leave(room);
+      }
     }
-    console.log("connecting to room ", id);
-    client.join(id);
     /* … */
   });
 
+  client.on("disconnect room", () => {
+    // for (room in client.rooms) {
+    //     client.to(room).emit("roommate disconnect", client.id);
+    //     client.leave(room);
+    // }
+    const clientRoom = getClientRoom(client);
+    client.leave(clientRoom);
+    client.to(clientRoom).emit("roommate disconnect", client.id);
+    delete clientRooms[client.id];
+  });
+
   client.on("cursor move", (position) => {
-    console.log("got position ", position);
     const { x, y } = position;
     clientPositions[client.id] = { x, y };
 
-    const clientRoom = Object.keys(client.rooms).find(
-      (room) => client.id !== room
-    );
+    const clientRoom = getClientRoom(client);
+    if (!clientRoom) return;
 
     client.to(clientRoom).emit("cursor move", client.id, [x, y]);
   });
 
   client.on("disconnect", () => {
     /* … */
+    console.log("disconnected client", client.id);
+    const clientRoom = clientRooms[client.id];
+
+    if (!clientRoom) return;
+
+    client.leave(clientRoom);
+    client.to(clientRoom).emit("roommate disconnect", client.id);
   });
 });
 
 server.listen(port);
+
+const getClientRoom = (client) => {
+  return Object.keys(client.rooms).find((room) => client.id !== room);
+};
