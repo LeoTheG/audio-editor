8a4814f60bcd16e460e3350d6f0eae23991f54b2
Leo Gharib <leonargharib@gmail.com>
2020-08-27 10:42:55 -0700

    server creates profiles

--- server/src/index.js
+++ server/src/index.js
@@ -3,20 +3,125 @@ const io = require("socket.io")(server);
 const port = process.env.PORT || 8000;
 
 const clientPositions = {};
+
 // {[clientId: string]: string}
 const clientRooms = {};
+const roomToClientProfiles = {};
+const clientProfiles = {};
+
+// { [clientId: string]: string }
+const selectedProfiles = {};
+
+// { [clientId: string]: string }
+const userNames = {};
+
+const profileOptions = {
+  prefixes: [
+    "amazing",
+    "deranged",
+    "charming",
+    "dapper",
+    "eager",
+    "defiant",
+    "spotted",
+    "rare",
+  ],
+  suffixes: [
+    "woodpecker",
+    "mallard",
+    "grasshopper",
+    "boar",
+    "snail",
+    "coyote",
+    "meerkat",
+    "narwhal",
+    "scorpion",
+  ],
+  avatars: ["kirby", "link", "mario", "nyancat", "ghost", "yoshi"],
+};
 
 io.on("connection", (client) => {
-  client.on("connect room", (id) => {
-    client.join(id);
-    clientRooms[client.id] = id;
+  if (!clientProfiles[client.id]) {
+    userNames[client.id] =
+      profileOptions.prefixes[
+        Math.floor(Math.random() * profileOptions.prefixes.length)
+      ] +
+      " " +
+      profileOptions.suffixes[
+        Math.floor(Math.random() * profileOptions.suffixes.length)
+      ];
+
+    const selectedProfilesValues = Object.values(selectedProfiles);
+
+    let newAvatar = "";
+
+    // basic attempt to prevent duplicate sprites, not checking for different rooms
+    if (selectedProfilesValues.length >= profileOptions.avatars.length) {
+      newAvatar =
+        profileOptions.avatars[
+          Math.floor(Math.random() * profileOptions.avatars.length)
+        ];
+    } else {
+      const availableAvatars = profileOptions.avatars.filter(
+        (avatar) => !selectedProfilesValues.includes(avatar)
+      );
+      newAvatar =
+        availableAvatars[Math.floor(Math.random() * availableAvatars.length)];
+    }
+
+    selectedProfiles[client.id] = newAvatar;
+
+    clientProfiles[client.id] = {
+      name: userNames[client.id],
+      avatar: newAvatar,
+    };
+  }
+
+  client.on("connect room", (roomId) => {
+    console.log("client " + client.id + " ===connected to room===" + roomId);
+    client.join(roomId);
+    if (
+      clientRooms[client.id] &&
+      roomToClientProfiles[clientRooms[client.id]][client.id]
+    ) {
+      delete roomToClientProfiles[clientRooms[client.id]][client.id];
+    }
+    clientRooms[client.id] = roomId;
+
+    if (!roomToClientProfiles[roomId]) {
+      roomToClientProfiles[roomId] = {};
+    }
+    const roomProfiles = { ...roomToClientProfiles[roomId] };
+
+    roomToClientProfiles[roomId][client.id] = clientProfiles[client.id];
+
+    console.log(
+      "room to client profiles for room ",
+      roomId,
+      " are ",
+      roomToClientProfiles[roomId]
+    );
+
     // leave old rooms
     for (room in client.rooms) {
-      if (client.id !== room && id !== room) {
+      if (client.id !== room && roomId !== room) {
         client.to(room).emit("roommate disconnect", client.id);
         client.leave(room);
+
+        if (roomToClientProfiles[room][client.id]) {
+          console.log(
+            "deleted old room profile for room and id",
+            room,
+            client.id
+          );
+          delete roomToClientProfiles[room][client.id];
+        }
       }
     }
+    client
+      .to(roomId)
+      .emit("profile info", client.id, clientProfiles[client.id]);
+    client.emit("room profile info", roomProfiles);
   });
 
   client.on("disconnect room", () => {
@@ -33,6 +138,7 @@ io.on("connection", (client) => {
     const clientRoom = getClientRoom(client);
     if (!clientRoom) return;
 
+    console.log("emitting cursor move for client and x y ", client.id, x, y);
     client.to(clientRoom).emit("cursor move", client.id, [x, y]);
   });
 
@@ -43,6 +149,11 @@ io.on("connection", (client) => {
 
     client.leave(clientRoom);
     client.to(clientRoom).emit("roommate disconnect", client.id);
+    if (clientRooms[client.id]) {
+      delete roomToClientProfiles[clientRooms[client.id]][client.id];
+      delete clientRooms[client.id];
+    }
+    delete clientProfiles[client.id];
   });
 });
 
